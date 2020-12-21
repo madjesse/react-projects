03c40a1e8eae1b6c5f966274f68f078b075a34d6
madjesse <jesseweijapan@gmail.com>
2020-12-21 10:37:10 +0900

    Finish project 4

--- 4-accordion/setup/src/Question.js
+++ 4-accordion/setup/src/Question.js
@@ -1,7 +1,18 @@
 import React, { useState } from 'react';
 import { AiOutlineMinus, AiOutlinePlus } from 'react-icons/ai';
-const Question = () => {
-  return <h2>question component</h2>;
+
+const Question = ({title, info}) => {
+  const [isUnfolded, setIsUnfolded] = useState(false);
+
+  return (
+    <div className="question">
+      <header>
+        <h4>{title}</h4>
+        {isUnfolded ? <AiOutlineMinus className="btn" onClick={() => setIsUnfolded(!isUnfolded)} /> : <AiOutlinePlus className="btn" onClick={() => setIsUnfolded(!isUnfolded)} />}
+      </header>
+      {isUnfolded && <p>{info}</p>}
+    </div>
+  );
 };
 
 export default Question;

fd95073a42c006f562bcfea6b197b1074fa58023
madjesse <jesseweijapan@gmail.com>
2020-12-21 13:40:51 +0900

    Finish project5

--- 5-menu/setup/src/Categories.js
+++ 5-menu/setup/src/Categories.js
@@ -1,7 +1,16 @@
 import React from 'react';
 
-const Categories = () => {
-  return <h2>categories component</h2>;
+const Categories = ({onClick}) => {
+  return (
+    <nav className="menu">
+      <ul className="btn-container">
+        <li><button className="filter-btn" value="all" onClick={onClick}>all</button></li>
+        <li><button className="filter-btn" value="breakfast" onClick={onClick}>breakfast</button></li>
+        <li><button className="filter-btn" value="lunch" onClick={onClick}>lunch</button></li>
+        <li><button className="filter-btn" value="shakes" onClick={onClick}>shakes</button></li>
+      </ul>
+    </nav>
+  );
 };
 
 export default Categories;
--- 5-menu/setup/src/Menu.js
+++ 5-menu/setup/src/Menu.js
@@ -1,7 +1,18 @@
 import React from 'react';
 
-const Menu = () => {
-  return <h2>menu component</h2>;
+const Menu = ({img, title, price, desc}) => {
+  return (
+    <div className="menu-item">
+      <img src={img} alt={title} className="photo" />
+      <div className="item-info">
+        <header>
+          <h4>{title}</h4>
+          <span className="price">{price}</span>
+        </header>
+        <p className="item-text">{desc}</p>
+      </div>
+    </div>
+  );
 };
 
 export default Menu;

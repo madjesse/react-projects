1dea1fd5926cb043b56de83d85ceb62e7a81e979
Jesse <jesseweijapan@gmail.com>
2020-12-24 10:58:58 +0900

    Start project 9

--- 9-color-generator/setup/src/SingleColor.js
+++ 9-color-generator/setup/src/SingleColor.js
@@ -1,8 +1,15 @@
 import React, { useState, useEffect } from 'react'
 import rgbToHex from './utils'
 
-const SingleColor = () => {
-  return <h4>single color</h4>
+const SingleColor = ({rgb, weight, index}) => {
+  const [alert, setAlert] = useState(false);
+  const bcg = rgb.join(',');
+  return (
+    <article className={`color`} style={{backgroundColor: `rgb(${bcg})`}}>
+      <p className="percent-value">{weight}%</p>
+      <p className="color-value">{rgbToHex(...rgb)}</p>
+    </article>
+  );
 }
 
-export default SingleColor
+export default SingleColor;

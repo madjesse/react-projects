cbb68bc7e99e56f3a6d533a8e916a9bb8cb49083
Jesse <jesseweijapan@gmail.com>
2020-12-27 11:35:53 +0900

    Finish project 10

--- 10-grocery-bud/setup/src/Alert.js
+++ 10-grocery-bud/setup/src/Alert.js
@@ -1,7 +1,15 @@
 import React, { useEffect } from 'react'
 
-const Alert = () => {
-  return <h2>alert component</h2>
+const Alert = ({msg, type, handleAlert, list}) => {
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      handleAlert();
+    }, 2000);
+    return () => clearTimeout(timeout);
+  }, [list]);
+  return (
+    <p className={`alert alert-${type}`}>{msg}</p>
+  );
 }
 
 export default Alert
